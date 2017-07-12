d626225852519cda576d4d30f5c63e9d109735cf
liweiyuan <535767595@qq.com>
2017-07-12 10:33:47 +0800

    commit

---
+++ spring_cloud_server/src/main/java/comtingyun/SpringCloudServerApplication.java
@@ -0,0 +1,17 @@
+package comtingyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudServerApplication {
+
+	public static void main(String[] args) {
+
+		//SpringApplication.run(SpringCloudServerApplication.class, args);
+		new SpringApplicationBuilder(SpringCloudServerApplication.class).web(true).run(args);
+	}
+}
