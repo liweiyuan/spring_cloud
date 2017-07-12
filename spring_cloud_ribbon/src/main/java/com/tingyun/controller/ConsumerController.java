40a7798eaba21173435576256c9d0281af26b96c
liweiyuan <535767595@qq.com>
2017-07-12 14:11:54 +0800

    commit

--- spring_cloud_ribbon/src/main/java/com/tingyun/controller/ConsumerController.java
+++ spring_cloud_ribbon/src/main/java/com/tingyun/controller/ConsumerController.java
@@ -1,10 +1,10 @@
 package com.tingyun.controller;
 
+import com.tingyun.service.ComputeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by tingyun on 2017/6/13.
@@ -12,12 +12,21 @@ import org.springframework.web.client.RestTemplate;
  */
 @RestController
 public class ConsumerController {
-    @Autowired
+    /*@Autowired
     private RestTemplate restTemplate;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add() {
         return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+    }*/
+
+
+    @Autowired
+    private ComputeService computeService;
+
+    @RequestMapping(value = "/add",method = RequestMethod.GET)
+    public String add(){
+        return computeService.addService();
     }
 
 }
---
+++ spring_cloud_ribbon/src/main/java/com/tingyun/service/ComputeService.java
@@ -0,0 +1,29 @@
+package com.tingyun.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by tingyun on 2017/7/12.
+ * 改造原来的服务消费方式，新增ComputeService类，在使用ribbon消费服务的
+ * 函数上增加@HystrixCommand注解来指定回调方法。
+ */
+@Service
+public class ComputeService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "addServiceFallBack")
+    public String addService(){
+        return restTemplate.getForEntity("COMPUTE-SERVICE/add?a=10&b=20",String.class).getBody();
+    }
+
+
+    public String addServiceFallBack(){
+        return "error";
+    }
+
+}
