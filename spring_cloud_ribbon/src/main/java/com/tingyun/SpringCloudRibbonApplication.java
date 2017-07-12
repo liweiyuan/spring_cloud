249fa43567aeea481e23ce09b961ab879994fff6
liweiyuan <535767595@qq.com>
2017-07-12 14:03:26 +0800

    commit

--- spring_cloud_ribbon/src/main/java/com/tingyun/SpringCloudRibbonApplication.java
+++ spring_cloud_ribbon/src/main/java/com/tingyun/SpringCloudRibbonApplication.java
@@ -2,6 +2,7 @@ package com.tingyun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient//注解来添加发现服务能力
 @SpringBootApplication
+@EnableCircuitBreaker//开启断路器的功能
 public class SpringCloudRibbonApplication {
 
 
