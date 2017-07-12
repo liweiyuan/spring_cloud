bd4f49e9a34eb21f15f68758d421342c670a4c6b
liweiyuan <535767595@qq.com>
2017-07-12 18:09:51 +0800

    commit

--- spring_cloud_feign/src/main/java/com/tingyun/component/ComputeClientHystrix.java
+++ spring_cloud_feign/src/main/java/com/tingyun/component/ComputeClientHystrix.java
@@ -1,7 +1,17 @@
 package com.tingyun.component;
 
+import com.tingyun.service.ComputeClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * Created by tingyun on 2017/7/12.
+ * 回调类ComputeClientHystrix，实现@FeignClient的接口，此时实现的方法就是对应@FeignClient接口中映射的fallback函数。
  */
-public class ComputeClientHystrix {
+@Component
+public class ComputeClientHystrix implements ComputeClient{
+    @Override
+    public Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
+        return -999;
+    }
 }
--- spring_cloud_feign/src/main/java/com/tingyun/service/ComputeClient.java
+++ spring_cloud_feign/src/main/java/com/tingyun/service/ComputeClient.java
@@ -1,5 +1,6 @@
 package com.tingyun.service;
 
+import com.tingyun.component.ComputeClientHystrix;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by tingyun on 2017/7/12.
  * 使用@FeignClient("compute-service")注解来绑定该接口对应compute-service服务
  */
-@FeignClient("compute-service")
+@FeignClient(value = "compute-service",fallback = ComputeClientHystrix.class)
 public interface ComputeClient {
 
     @RequestMapping(value = "/add",method = RequestMethod.GET)
